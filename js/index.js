150544b3d77b94af75db244822a2af68e99cab3a
gbpool <gbpoolgordo@outlook.com>
2024-11-06 14:31:13 -0300

    update 3

---
+++ js/index.js
@@ -0,0 +1,7 @@
+const BtnDialogCatedral = document.getElementById("botao-catedral");
+const dialogCatedral = document.getElementById("Catedral");
+
+
+BtnDialogCatedral.addEventListener("click", () => {
+    dialogCatedral.showModal();
+})
